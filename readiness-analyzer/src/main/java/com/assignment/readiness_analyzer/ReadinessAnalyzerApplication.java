13e852ff32320088653e1c41c00d434d930b85ec
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-10-03 16:00:59 +0530

    build: initialise project with Spring Boot

---
+++ readiness-analyzer/src/main/java/com/assignment/readiness_analyzer/ReadinessAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.readiness_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinessAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadinessAnalyzerApplication.class, args);
+	}
+
+}
---
+++ readiness-analyzer/src/test/java/com/assignment/readiness_analyzer/ReadinessAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.readiness_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinessAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
